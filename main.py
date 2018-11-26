f02d687001622d98dc143a9a62185f82a50a7bb9
Gautham Kumar <heygautham@gmail.com>
2018-11-27 01:01:20 +0530

    Basic video choosing functionality complete

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ mysql = MySQL(app)
 def loginPage():
     return render_template('index.html')
 
-@app.route('/watch/<videoID>')
-def watchPage(videoID):
+@app.route('/watch/<roomID>')
+def watchPage(roomID):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT videoID FROM open_rooms WHERE roomID={}".format(roomID))
+    data = cursor.fetchone()
+    videoID = data[0]
     return render_template('watch.html', videoID = videoID)
 
 @app.route('/room/check')
@@ -32,7 +37,7 @@ def checkIfRoomExists():
             status=401
         ),401)
 
-    cursor.execute("SELECT * from open_rooms where RoomID='{}'".format(roomID))
+    cursor.execute("SELECT * FROM open_rooms WHERE RoomID='{}'".format(roomID))
     data = cursor.fetchone()
 
     if bool(data):
@@ -80,6 +85,7 @@ def addNewRoom():
 
     return jsonify(
         response="Room successfully created",
+        roomID=roomID,
         status=200
     )
 
