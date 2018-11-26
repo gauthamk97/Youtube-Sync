da6a4b0d1e74ab7e4bf98655c3f79db2e5899106
Gautham Kumar <heygautham@gmail.com>
2018-11-27 00:26:05 +0530

    Added two routes
    1. Check if room exists
    2. Add a new room

--- main.py
+++ main.py
@@ -1,11 +1,89 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, jsonify, make_response, request
+from flask_socketio import SocketIO
+from flaskext.mysql import MySQL
+from pymysql.err import IntegrityError
+
 app = Flask(__name__)
+socketIO = SocketIO(app)
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def helloworld(name=None):
-    return render_template('hello.html', name=name)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'idk'
+app.config['MYSQL_DATABASE_DB'] = 'youtube_sync'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql = MySQL(app)
 
-with app.test_request_context():
-    print("Hello There")
-    print(url_for('helloworld', name='gautham'))
+@app.route('/')
+def loginPage():
+    return render_template('index.html')
+
+@app.route('/watch/<videoID>')
+def watchPage(videoID):
+    return render_template('watch.html', videoID = videoID)
+
+@app.route('/room/check')
+def checkIfRoomExists():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    roomID = request.args.get('roomID')
+
+    if roomID is None:
+        return make_response(jsonify(
+            response="Parameters missing",
+            status=401
+        ),401)
+
+    cursor.execute("SELECT * from open_rooms where RoomID='{}'".format(roomID))
+    data = cursor.fetchone()
+
+    if bool(data):
+        return make_response(jsonify(
+            response="Room exists",
+            status=200
+        ), 200)
+    else:
+        return make_response(jsonify(
+            response="Room doesn't exist",
+            status=500
+        ), 500)
+
+@app.route('/room/add')
+def addNewRoom():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    videoID = request.args.get('videoID')
+
+    if videoID is None:
+        return make_response(jsonify(
+            response="Parameters missing",
+            status=401
+        ),401)
+
+    cursor.execute("SELECT MAX(roomID) FROM open_rooms")
+    data = cursor.fetchall()
+    maxValue = data[0][0]
+
+    if not bool(maxValue):
+        # No rooms exist
+        roomID = 1000
+    else:
+        roomID = int(maxValue)+1
+
+    try:
+        cursor.execute("INSERT INTO open_rooms VALUES ('{}', '{}');".format(roomID, videoID))
+        conn.commit()
+    except IntegrityError:
+        # Should never happen since roomID = max(old room IDs)+1
+        return make_response(jsonify(
+            response="Room already exists",
+            status=500
+        ),500)
+
+    return jsonify(
+        response="Room successfully created",
+        status=200
+    )
+
+@socketIO.on('my event')
+def receivedMessage(json, methods=['POST', 'GET']):
+    print(json)
+    socketIO.emit('Receieved message')
