ae0e63eb9b6b662b57cc1b73164e665a3f1005c1
Gautham Kumar <heygautham@gmail.com>
2018-11-28 13:39:43 +0530

    Added sessions and username

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, url_for, render_template, jsonify, make_response, request
-from flask_socketio import SocketIO
+from flask import Flask, url_for, render_template, jsonify, make_response, request, session, redirect
+from flask_socketio import SocketIO, join_room, leave_room, close_room, rooms
 from flaskext.mysql import MySQL
 from pymysql.err import IntegrityError
 
 app = Flask(__name__)
 socketIO = SocketIO(app)
 
+app.config['SECRET_KEY'] = 'thiscanbeanythinglol'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'idk'
 app.config['MYSQL_DATABASE_DB'] = 'youtube_sync'
@@ -13,46 +14,35 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql = MySQL(app)
 
 @app.route('/')
-def loginPage():
+def homePage():
     return render_template('index.html')
 
-@app.route('/watch/<roomID>')
-def watchPage(roomID):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT videoID FROM open_rooms WHERE roomID={}".format(roomID))
-    data = cursor.fetchone()
-    videoID = data[0]
-    return render_template('watch.html', videoID = videoID)
-
-@app.route('/room/check')
-def checkIfRoomExists():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    roomID = request.args.get('roomID')
-
-    if roomID is None:
+@app.route('/login')
+def login():
+    username = request.args.get('username')
+    if username is None:
         return make_response(jsonify(
-            response="Parameters missing",
-            status=401
-        ),401)
-
-    cursor.execute("SELECT * FROM open_rooms WHERE RoomID='{}'".format(roomID))
-    data = cursor.fetchone()
-
-    if bool(data):
-        return make_response(jsonify(
-            response="Room exists",
-            status=200
-        ), 200)
+            response="Missing username parameter"
+        ), 401)
     else:
+        session['username'] = username
         return make_response(jsonify(
-            response="Room doesn't exist",
-            status=500
-        ), 500)
+            response="Session successfully created"
+        ), 200)
+
+@app.route('/logout')
+def logout():
+    # Clear session if it exists
+    session.pop('username', None)
+    if 'username' not in session:
+        return redirect(url_for('homePage'))
 
 @app.route('/room/add')
 def addNewRoom():
+    # Session Check
+    if 'username' not in session:
+        return redirect(url_for('homePage'))
+
     conn = mysql.connect()
     cursor = conn.cursor()
     videoID = request.args.get('videoID')
@@ -60,7 +50,6 @@ def addNewRoom():
     if videoID is None:
         return make_response(jsonify(
             response="Parameters missing",
-            status=401
         ),401)
 
     cursor.execute("SELECT MAX(roomID) FROM open_rooms")
@@ -80,16 +69,49 @@ def addNewRoom():
         # Should never happen since roomID = max(old room IDs)+1
         return make_response(jsonify(
             response="Room already exists",
-            status=500
         ),500)
 
-    return jsonify(
+    return make_response(jsonify(
         response="Room successfully created",
         roomID=roomID,
-        status=200
-    )
+    ), 200)
 
-@socketIO.on('my event')
-def receivedMessage(json, methods=['POST', 'GET']):
-    print(json)
-    socketIO.emit('Receieved message')
+@app.route('/room/check')
+def checkIfRoomExists():
+    # Session Check
+    if 'username' not in session:
+        return redirect(url_for('homePage'))
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    roomID = request.args.get('roomID')
+
+    if roomID is None:
+        return make_response(jsonify(
+            response="Parameters missing",
+        ),401)
+
+    cursor.execute("SELECT * FROM open_rooms WHERE RoomID='{}'".format(roomID))
+    data = cursor.fetchone()
+
+    if bool(data):
+        return make_response(jsonify(
+            response="Room exists",
+        ), 200)
+    else:
+        return make_response(jsonify(
+            response="Room doesn't exist",
+        ), 500)
+
+@app.route('/watch/<roomID>')
+def watchPage(roomID):
+    # Session Check
+    if 'username' not in session:
+        return redirect(url_for('homePage'))
+    
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT videoID FROM open_rooms WHERE roomID={}".format(roomID))
+    data = cursor.fetchone()
+    videoID = data[0]
+    return render_template('watch.html', videoID = videoID, roomID = roomID)
